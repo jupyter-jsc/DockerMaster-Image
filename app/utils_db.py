a6eb7060aa1677982784dd40bb88bf19fc4695dd
Tim Kreuzer <t.kreuzer@fz-juelich.de>
2020-03-27 10:45:12 +0100

    bugfixes

--- app/utils_db.py
+++ app/utils_db.py
@@ -8,6 +8,7 @@ from contextlib import closing
 import psycopg2
 
 def get_user_id(app_logger, uuidcode, database, email):
+    email = email.replace("@", "_at_")
     with closing(psycopg2.connect(host=database.get('host'),
                                   port=database.get('port'),
                                   user=database.get('user'),
@@ -15,7 +16,7 @@ def get_user_id(app_logger, uuidcode, database, email):
                                   database=database.get('database'))) as con: # auto closes
         with closing(con.cursor()) as cur: # auto closes
             with con: # auto commit
-                cmd = "SELECT id FROM User WHERE email = %s"
+                cmd = "SELECT \"id\" FROM \"User\" WHERE \"email\" = %s"
                 app_logger.trace("uuidcode={} - Execute: {}, email={}".format(uuidcode, cmd, email))
                 cur.execute(cmd,
                             (email, ))
@@ -28,6 +29,7 @@ def get_user_id(app_logger, uuidcode, database, email):
 
 
 def create_user(app_logger, uuidcode, database, email):
+    email = email.replace("@", "_at_")
     with closing(psycopg2.connect(host=database.get('host'),
                                   port=database.get('port'),
                                   user=database.get('user'),
@@ -35,7 +37,7 @@ def create_user(app_logger, uuidcode, database, email):
                                   database=database.get('database'))) as con: # auto closes
         with closing(con.cursor()) as cur: # auto closes
             with con: # auto commit
-                cmd = "INSERT INTO User (email) VALUES (%s)"
+                cmd = "INSERT INTO \"User\" (\"email\") VALUES (%s)"
                 app_logger.trace("uuidcode={} - Execute: {}, email={}".format(uuidcode, cmd, email))
                 cur.execute(cmd,
                             (email, ))
@@ -49,7 +51,7 @@ def get_next_slave(app_logger, uuidcode, database):
                                   database=database.get('database'))) as con: # auto closes
         with closing(con.cursor()) as cur: # auto closes
             with con: # auto commit
-                cmd = "SELECT id, Hostname FROM DockerSpawner ORDER BY Running LIMIT 1"
+                cmd = "SELECT \"id\", \"Hostname\" FROM \"DockerSpawner\" ORDER BY \"Running\" LIMIT 1"
                 app_logger.trace("uuidcode={} - Execute: {}".format(uuidcode, cmd))
                 cur.execute(cmd)
                 results = cur.fetchall()
@@ -67,8 +69,8 @@ def insert_container(app_logger, uuidcode, database, userid, slaveid, servername
                                   database=database.get('database'))) as con: # auto closes
         with closing(con.cursor()) as cur: # auto closes
             with con: # auto commit
-                cmd = "INSERT INTO Containers (user_id, slave_id, uuid, name) VALUES (%s, %s, %s, %s)"
-                app_logger.trace("uuidcode={} - Execute: {}".format(uuidcode, cmd))
+                cmd = "INSERT INTO \"Containers\" (\"user_id\", \"slave_id\", \"uuid\", \"name\") VALUES (%s, %s, %s, %s)"
+                app_logger.trace("uuidcode={} - Execute: {} with {} {} {} {}".format(uuidcode, cmd, userid, slaveid, uuidcode, servername))
                 cur.execute(cmd,
                             (userid,
                              slaveid,
@@ -83,8 +85,8 @@ def get_user_running(app_logger, uuidcode, database, user_id):
                                   database=database.get('database'))) as con: # auto closes
         with closing(con.cursor()) as cur: # auto closes
             with con: # auto commit
-                cmd = "SELECT COUNT(id) FROM Containers WHERE user_id = %s"
-                app_logger.trace("uuidcode={} - Execute: {}".format(uuidcode, cmd))
+                cmd = "SELECT COUNT(\"id\") FROM \"Containers\" WHERE \"user_id\" = %s"
+                app_logger.trace("uuidcode={} - Execute: {} with {}".format(uuidcode, cmd, user_id))
                 cur.execute(cmd,
                             (user_id, ))
                 results = cur.fetchall()
@@ -102,8 +104,8 @@ def increase_slave_running(app_logger, uuidcode, database, slave_id):
                                   database=database.get('database'))) as con: # auto closes
         with closing(con.cursor()) as cur: # auto closes
             with con: # auto commit
-                cmd = "UPDATE DockerSpawner SET Running = Running + 1 WHERE id = %s"
-                app_logger.trace("uuidcode={} - Execute: {}".format(uuidcode, cmd))
+                cmd = "UPDATE \"DockerSpawner\" SET \"Running\" = \"Running\" + 1 WHERE \"id\" = %s"
+                app_logger.trace("uuidcode={} - Execute: {} with {}".format(uuidcode, cmd, slave_id))
                 cur.execute(cmd,
                             (slave_id, ))
 
@@ -116,8 +118,8 @@ def get_container_info(app_logger, uuidcode, database, user_id, servername):
                                   database=database.get('database'))) as con: # auto closes
         with closing(con.cursor()) as cur: # auto closes
             with con: # auto commit
-                cmd = "SELECT slave_id, uuid FROM Containers WHERE user_id = %s AND name = %s"
-                app_logger.trace("uuidcode={} - Execute: {}".format(uuidcode, cmd))
+                cmd = "SELECT \"slave_id\", \"uuid\" FROM \"Containers\" WHERE \"user_id\" = %s AND \"name\" = %s"
+                app_logger.trace("uuidcode={} - Execute: {} with {} {}".format(uuidcode, cmd, user_id, servername))
                 cur.execute(cmd,
                             (user_id, servername))
                 results = cur.fetchall()
@@ -127,7 +129,7 @@ def get_container_info(app_logger, uuidcode, database, user_id, servername):
     if len(results) > 0:
         return results[0][0], results[0][1]
     else:
-        return 0, ""
+        return []
     
 def get_slave_hostname(app_logger, uuidcode, database, slave_id):
     with closing(psycopg2.connect(host=database.get('host'),
@@ -137,8 +139,8 @@ def get_slave_hostname(app_logger, uuidcode, database, slave_id):
                                   database=database.get('database'))) as con: # auto closes
         with closing(con.cursor()) as cur: # auto closes
             with con: # auto commit
-                cmd = "SELECT Hostname FROM DockerSpawner WHERE id = %s"
-                app_logger.trace("uuidcode={} - Execute: {}".format(uuidcode, cmd))
+                cmd = "SELECT \"Hostname\" FROM \"DockerSpawner\" WHERE \"id\" = %s"
+                app_logger.trace("uuidcode={} - Execute: {} with {}".format(uuidcode, cmd, slave_id))
                 cur.execute(cmd,
                             (slave_id, ))
                 results = cur.fetchall()
@@ -148,6 +150,7 @@ def get_slave_hostname(app_logger, uuidcode, database, slave_id):
     if len(results) > 0:
         return results[0][0]
     else:
+        app_logger.error("uuidcode={} - Result from Database get_slave_hostname: {}".format(uuidcode, results))
         return 0
     
 
@@ -159,8 +162,8 @@ def decrease_slave_running(app_logger, uuidcode, database, slave_id):
                                   database=database.get('database'))) as con: # auto closes
         with closing(con.cursor()) as cur: # auto closes
             with con: # auto commit
-                cmd = "UPDATE DockerSpawner SET Running = Running - 1 WHERE id = %s"
-                app_logger.trace("uuidcode={} - Execute: {}".format(uuidcode, cmd))
+                cmd = "UPDATE \"DockerSpawner\" SET \"Running\" = \"Running\" - 1 WHERE \"id\" = %s"
+                app_logger.trace("uuidcode={} - Execute: {} with {}".format(uuidcode, cmd, slave_id))
                 cur.execute(cmd,
                             (slave_id, ))
                 
@@ -172,8 +175,8 @@ def remove_container(app_logger, uuidcode, database, user_id, servername):
                                   database=database.get('database'))) as con: # auto closes
         with closing(con.cursor()) as cur: # auto closes
             with con: # auto commit
-                cmd = "DELETE FROM Containers WHERE user_id = %s AND name = %s"
-                app_logger.trace("uuidcode={} - Execute: {}".format(uuidcode, cmd))
+                cmd = "DELETE FROM \"Containers\" WHERE \"user_id\" = %s AND \"name\" = %s"
+                app_logger.trace("uuidcode={} - Execute: {} with {} {}".format(uuidcode, cmd, user_id, servername))
                 cur.execute(cmd,
                             (user_id, servername ))
 
